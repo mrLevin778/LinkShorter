8948779cab3af865d61ee17623425a1d1cf4f6c5
mrLevin778 <vitaliy.respect@gmail.com>
2022-11-08 23:17:34 +0200

    Added model Shorter and make migrations

--- linkshorter/linkshorterapp/models.py
+++ linkshorter/linkshorterapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Shorter(models.Model):
+    long_link = models.CharField(max_length=200, null=False, help_text="Copy here your loooong link")
+    short_link = models.CharField(max_length=30)
+    days_alive = models.IntegerField(max_length=3)
+    slug = models.SlugField(max_length=7, auto_created=True)
+    is_expired = models.BooleanField(default=False)
